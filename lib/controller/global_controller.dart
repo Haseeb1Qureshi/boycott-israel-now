61adcc7019c080fb91c747107c9371e890390da0
Haseeb Qureshi <haseebqureshi836@gnail.com>
2024-09-01 10:40:46 +0500

    chnages

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -45,7 +45,7 @@ class GlobalController extends GetxController {
             .map((item) => Categoryy.fromJson(item as Map<String, dynamic>))
             .toList();
         filteredCategories.value = categories;
-        Get.snackbar('Successfully', 'Data checked Successfully');
+        Get.snackbar('Successfully', 'Data fetching Successfully');
       } else {
         Get.snackbar(
           'Error',
